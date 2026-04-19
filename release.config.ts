17a25d1043c069c7dadbb5e23f4034d137771d71
GMarinov2 <marinov.georg97@gmail.com>
2026-04-20 01:42:48 +0300

    make semantic release bump package.json version semver:minor

--- release.config.ts
+++ release.config.ts
@@ -1,24 +1,34 @@
 import type { GlobalConfig } from 'semantic-release';
 
 const config: GlobalConfig = {
-    branches: ['main'],
-    plugins: [
-        ['@semantic-release/commit-analyzer', {
-            preset: 'angular',
-            releaseRules: [
-                { message: '*semver:major*', release: 'major' },
-                { message: '*semver:minor*', release: 'minor' },
-                { message: '*semver:patch*', release: 'patch' },
-                { message: '*', release: 'patch' },
-            ],
-        }],
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        '@semantic-release/git',
-        '@semantic-release/github',
-    ],
-    repositoryUrl: 'https://github.com/hypermarinov/lucidlink-assignment.git',
-    tagFormat: 'v${version}',
+	branches: ['main'],
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'angular',
+				releaseRules: [
+					{ message: '*semver:major*', release: 'major' },
+					{ message: '*semver:minor*', release: 'minor' },
+					{ message: '*semver:patch*', release: 'patch' },
+					{ message: '*', release: 'patch' },
+				],
+			},
+		],
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		['@semantic-release/npm', { npmPublish: false }],
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json', 'CHANGELOG.md'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]',
+			},
+		],
+		'@semantic-release/github',
+	],
+	repositoryUrl: 'https://github.com/hypermarinov/lucidlink-assignment.git',
+	tagFormat: 'v${version}',
 };
 
 export default config;
